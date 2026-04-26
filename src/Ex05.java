5e66d96ccbf46b73852d7655b52e7cf9f632b9ea
lua-tech <141778759+lua-tech@users.noreply.github.com>
2026-04-26 15:30:37 -0300

    feat: clear ArrayList elements using clear method

---
+++ src/Ex05.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex05 {
+
+    public static void main(String [] args) {
+
+    /* 5) Crie umArrayList chamado "elementos" e adicione alguns elementos. Limpe
+    o ArrayList, removendo todos os elementos */
+
+    List<String> elementos = new ArrayList<String>();
+
+    // adicionar elementos
+    elementos.add("Cálcio");
+    elementos.add("Bario");
+    elementos.add("Ferro");
+    elementos.add("Carbono");
+
+    // remover os elementos
+    elementos.clear();
+
+    }
+
+}
