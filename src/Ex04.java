5236416650d3708dab153fb4406a673cd01b9d62
lua-tech <141778759+lua-tech@users.noreply.github.com>
2026-04-26 15:19:08 -0300

    feat: implement ArrayList to store words and print its size

--- src/Ex04.java
+++ src/Ex04.java
@@ -3,7 +3,19 @@ import java.util.List;
 
 public class Ex04 {
 
-    // 4) Crie um ArrayList chamado "palavras"e adicione algumas palavras.
-    //Exiba o tamanho do ArrayList
+    public static void main(String [] args) {
+
+        /* 4) Crie um ArrayList chamado "palavras" e adicione algumas palavras.
+        Exiba o tamanho do ArrayList. */
+
+        List<String> palavras = new ArrayList<String>();
+
+        palavras.add("Saosin");
+        palavras.add("Silverstein");
+        palavras.add("Alexisonfire");
+
+        System.out.println(palavras.size());
+
+    }
 
 }
